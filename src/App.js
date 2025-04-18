a769ee143ef8ccb917603c9eab0f6aba926119eb
Thomas Alemu <thomasalemu@macbookair.lan>
2025-04-18 01:09:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
+
+const posts = [
+  {
+    slug: "on-attention",
+    title: "On Attention",
+    content: `We rarely ask what we are paying with when we pay attention. The cost is often deeper than we think. True attention requires energy, patience, and the ability to say no.`
+  },
+  {
+    slug: "chapel",
+    title: "Chapel",
+    content: `May 4, 2022
+
+Rock With You - Michael Jackson (0:00--)
+All Falls Down - Kanye West (0:00--)
+
+Part 0: A Prologue
+I would like to start my chapel today, with what I hope to accomplish. I’m sure there is a great story within me that puts things in perspective, and gives some tangible, quotable, takeaway from this chapel. If you’d like to know the in’s and out’s of my time at CA, what classes I’d recommend, or the lessons I learned from some mistake I begrudgingly hold onto, feel free to email me. However, I hope instead you get a better understanding of how I am able to be up here today. Even if you can’t relate to a word I say, I hope you take my chapel as a vessel to be more thoughtful on your own journey as to how you are able to be here with me.  
+
+Pity (feat. J Cole) - Voli Contra (0:00-0:45)
+
+Part 1: My Birth
+I was born on Thursday July 15, 2004, at Beth Israel Hospital in Boston. The third and final son for my parents, I am 4 and 5 years apart from each of my brothers. I was an American, my birth certificate decreed that day. Born to two American-Ethiopian parents, each of them represent a form of the American Dream in their own right. My birth retraces its steps throughout both of their lives.
+
+I am born from my father. He was one of 12 kids born to his family in Ethiopia.  However, his household was always shifting in size due to the generosity of my grandparents, who were welcoming neighborhood kids in and out of their home whenever they needed a place to stay or a family to be surrounded by. My father was right in the middle of the pack, however as he grew older and his father passed away, he had to mature quickly, needing to provide for his family. At this same time in his teenage years, Ethiopia was facing political unrest, young activists were hoping to unseat the Ethiopian royal family. This movement was quickly hijacked and turned violent, soon becoming a tragic years long terror. Relying on luck and the generosity of others, he was able to seek safety in Djibouti, subsequently traveling halfway around the world to seek asylum in the United States. 
+
+Although my father was independent throughout his journey, and later was able to support and bring much of his family to the United States. He recounts the countless people who made it possible for him to immigrate safely and build himself a new life in the United States. People like the family in Djibouti who welcomed him into their home, the gentle customs officer who allowed my teenage dad to properly enter the United States, or the family friend who picked him up from the airport when he knew nobody else in Boston, are integral to any immigrant story’s existence. 
+
+I am born from my mother. She fondly tells me about her grandparents house, which has now been renovated and turned into a hotel in Addis if you’d like to see it, where she and lots of her extended family grew up together. She attended a French International School in the city, where she learned to speak French fluently and was able to get a scholarship to study in France for college. She and the few other Ethiopians who had the opportunity to study abroad would have to spend weeks and months there alone as they couldn’t afford to travel back home during breaks. After spending some years back in Ethiopia after graduating, my mother decided to try to move to the US after hearing of some relatives who did so themselves. She was able to move to Boston with her mother, and settle here, finding a job at a local school. 
+
+My birth is one of many first-generation American stories, those that are over glorified and romanticized today. One that is fleeting, but still drives America to define itself as a melting pot of improbable success. My birth is impossible without this pursuit for a land of seemingly limitless opportunity. My birth hinges on the everlasting work ethic and persistence of my parents and many other immigrants just like them who paved this path. I owe a debt of gratitude to those individuals who built the idea that lives like mine can not only exist in America, but flourish and be America. 
+
+To Mami and Abeba: You have done so much for Markos, Daniel, and I. Every day I am reminded of how hard working you both are, and how fortunate we are to have such loving and caring parents. I love you. 
+
+Ethiopia - Teddy Afro (0:00-0:40)
+
+Part 2: The Internet
+
+This past fall, on a brisk sunny day, rumor was spreading around the senior section of an anti-vax protest happening in Concord Center. Intrigued, Shihab Moral and I decided to walk over and watch what was unfolding.  The crowd of middle aged adults brandished signs with an array of inflammatory statements about “protecting their children from the vaccine” or how “they aren’t the government’s lab rats”. Although the town center seemed a bit distraught from this, for whatever reason Shihab and I decided to approach the crowd to talk to them. Our discussion started with specifically understanding why they opposed vaccine mandates, however it evolved into a greater conversation on inequality in the United States. They weren’t screaming, they weren’t angry, but all I remember is that they were scared. Scared of what the future was to bring, scared of what they were witnessing right there coming.
+
+However, they quickly lost touch with our conversation. We could see how their fear was exacerbated by facebook conspiracy theories and QAnon stories they insisted on sharing with Shihab and I. They repeated the chants I’ve heard too often online, entrenching them in a cycle of distrust far removed from the world that surrounds me. 
+
+Having spent so much time staring at a screen for the past two years, my outlook on life and political discourse has been skewed by the internet. Walking away from that moment, in some contorted way I saw myself in them. I couldn’t help but think if I am victim to the same mechanisms of the internet that they were so clearly failing to recognize themselves. Are the tools that drove their fear and their protest shaping me to fundamentally disagree with them?
+ 
+They’re feelings were no different from mine, their agitation and restlessness with the political climate around them is identical to what I have felt so often, yet social media has codified our experiences into becoming one that revolves around dividing ourselves into any sort of binary. Creating an us versus them mentality, and with every click of a new page or swipe on our phone we shape the “us” and the “them”. Value is extracted on rigidizing the walls of this binary, where we no longer see each other for our nuances. Instead, we catch glimpses of one another to construct preconceptions of the world around us. We entrust the internet to pass judgment, create relationships, and brew hatred. Any time we do this, we are trusting that our experience is real, that the moment is a shared one wherever we choose to send it off to. 
+
+My life has been immeasurably impacted by this, forming and understanding my own self is impossible without acknowledging the reality of how omnipresent the internet is. The internet has changed my self-perception, where instead of defining myself and my worth objectively, I instead place myself relative to my surroundings and the lens of the world I am exposed to. I see who is prettier or uglier, who is smarter or dumber, told who’s opinion is fact or what fact is opinion. 
+
+It has been normalized to constantly consume and internalize the internet, to the point where we can no longer discern reality from its inauthentic and incomplete counterpart. I recognize that I am in one way or another an aggregate of my experience on the internet, subconsciously I embody it, quoting voices I’ve never heard, objectifying faces I’ve never seen, and desiring lives that don’t truly exist.
+
+ i (Album Version) - Kendrick Lamar  (0:20-0:58)
+
+Part 3: Thank-yous
+
+To Markos: I’ve looked up to you in so many ways throughout my whole life, I am so glad to have an older brother like you who’s steps I can follow in. I love you. 
+
+To Daniel: You are the smartest person I know in my life.  Whenever I feel a bit lost, you are able to be there to ground me and help. Your resilience always amazes me. I love you.
+
+To all the adults at CA who’ve taught me: You have all served such an important role in my growth here, and given me the tools to be more thoughtful and empathetic. Thank you all.
+
+To John: I think CA should be more worried that their director of Financial Aid is a fan of cryptocurrencies. Thank you for your guidance in the past 4 years and always advocating for me. I look forward to all our meetings, and it is truly a pleasure whenever I am able to talk to you. 
+
+To Track: Thank you for all the fun for the past 4 years. Shoutout to my fellow 4x1 boys, Nick, Audeep, and Chris, and to everyone here make sure to go follow @spearkidz.v2 on instagram. Shihab’s been grinding the account. Here’s to a great meet today.
+
+To my friends: I genuinely have no idea what I would do without each and every one of you. The love and support you all show me has truly been beyond my dreams.
+And a BIG shout out to Ishan Narra today, it’s May the 4th (May the Fourth be with you), I know you are a big Star Trek guy so kill some cling clongs for me today or whatever. I’ve never seen the movies.
+
+Can’t Take My Eyes Off of You - Ms. Lauryn Hill (0:30-1:25)
+
+
+{PART 3.5 TO BE READ TO BENCH QUIETLY DURING THIS MUSIC BREAK}
+Some freshman confessions I have to make about y’all
+One time freshman year, Gio saw that I was listening to Sicko Mode at breakfast, and said “This man really listening to sicko mode unironically bruh”. I have never recovered.
+For the first half of Freshman year, I thought Jarrett was Kim Kopelman’s son.
+The first time I hung out at a CA friend's house was at Jack’s with Benoit and Andy, for a nice fall Fortnite session. We didn’t even hang out or anything, we each found a different room or corner in Jack’s house and played Fortnite as if we were each in our own respective homes. Good ole’ days.
+At Freshman Orientation, I recognized Sam Weitzman-Kurker in the SHAC shooting hoops, because my middle school would play his. Before our game, he would clap very loudly in our faces hyping himself up. We went on to whoop their team’s ass.
+
+Part 4: The End
+
+I am still on this journey of understanding what defining myself really means. I will never know truly what my actions and my character are really a product of, where the boundaries of my identity ends and the insincerity of imitation begin. But somewhere in all this mist I am certain that I am in part made up of those who I love, those who I imitate, and those who I idolize. 
+
+I would like to end my chapel by sharing a quote I heard in my Astronomy class this fall from the great astronomer Carl Sagan. He said this when NASA's Voyager 1 orbital took an image of Earth as it passed by Neptune. In this photo, Earth is reduced down to a pixel, simply a small spec in between the vastness of our Solar System. 
+
+“Look again at that dot. That's here. That's home. That's us. On it everyone you love, everyone you know, everyone you ever heard of, every human being who ever was, lived out their lives. The aggregate of our joy and suffering, thousands of confident religions, ideologies, and economic doctrines, every hunter and forager, every hero and coward, every creator and destroyer of civilization, every king and peasant, every young couple in love, every mother and father, hopeful child, inventor and explorer, every teacher of morals, every corrupt politician, every "superstar," every "supreme leader," every saint and sinner in the history of our species lived there. There is perhaps no better demonstration of the folly of human conceits than this distant image of our tiny world. It underscores our responsibility to deal more kindly with one another, and to preserve and cherish the pale blue dot, the only home we've ever known.” 
+
+My name is Thomas Wandossan Alemu and that was my chapel. 
+
+Thank you for being here everyone. 
+
+Spice Girl- Aminé (0:00--)
+`
+  }
+];
+
+function PostPage() {
+  const { slug } = useParams();
+  const post = posts.find((p) => p.slug === slug);
+
+  if (!post) return <p>Post not found.</p>;
+
+  return (
+    <article className="whitespace-pre-wrap">
+      <h2 className="text-xl font-bold mb-4">{post.title}</h2>
+      <p>{post.content}</p>
+    </article>
+  );
+}
+
+function AboutPage() {
+  return (
+    <section>
+      <h2 className="text-xl font-bold mb-2">Welcome to Tomiye Talks</h2>
+      <p>This is a space for essays, thoughts, and reflections. More coming soon.</p>
+    </section>
+  );
+}
+
+export default function Blog() {
+  return (
+    <Router>
+      <div
+        className="min-h-screen bg-white"
+        style={{ fontFamily: "Times New Roman, serif", fontSize: "12pt" }}
+      >
+        <main className="grid grid-cols-4 gap-10 px-12 py-10">
+          <aside className="col-span-1 border-r border-gray-300 pr-6">
+            <div className="text-4xl font-bold mb-8">Tomiye Talks</div>
+            <nav className="flex flex-col gap-4 text-base">
+              <Link
+                to="/"
+                className="text-black no-underline hover:underline hover:text-blue-600"
+              >
+                About
+              </Link>
+              {posts.map((post) => (
+                <Link
+                  key={post.slug}
+                  to={`/essay/${post.slug}`}
+                  className="text-black no-underline hover:underline hover:text-blue-600"
+                >
+                  {post.title}
+                </Link>
+              ))}
+            </nav>
+          </aside>
+
+          <section className="col-span-3">
+            <Routes>
+              <Route path="/" element={<AboutPage />} />
+              <Route path="/essay/:slug" element={<PostPage />} />
+            </Routes>
+          </section>
+        </main>
+      </div>
+    </Router>
+  );
+}
